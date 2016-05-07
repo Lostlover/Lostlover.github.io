4df91eda94daa023f4cb10c9e40425d026ac6b41
Lostlover <497278979@qq.com>
2016-05-07 17:39:46 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,38 @@
 //配置require模块引用
 require.config({
-    baseUrl:"/app/scripts",
+    baseUrl: "/app/scripts",
     paths: {
         angular: "vendor/angular/angular.min",
         domReady: "vendor/require/domReady",
+        css: "vendor/require/css",
+        text: "vendor/require/text",
         angularResource: "vendor/angular/angular-resource.min",
-        jquery: "vendor/jquery/jquery"
+        jquery: "vendor/jquery/jquery",
+        bootstrap: "vendor/bootstrap/js/bootstrap.min"
     },
     shim: {
         angularResource: {
             deps: ["angular"]
         },
+        bootstrap: {
+            deps: ["jquery","css!vendor/bootstrap/css/bootstrap.min"],
+            exports: "bootstrap"
+        },
         angular: {
-            deps: ["jquery"],
+            deps: ["jquery", "bootstrap","css!../../style/css/global.css","css!../../style/css/style.css"],
             exports: "angular"
-        }
+        },
+
     }
 });
 //配置angular
 require(['app',
     'controllers/mainController',
     'services/testService',
-    'directives/testDirective'], function (angular,app) {
+    'directives/testDirective'], function (angular, app) {
     'use strict';
     console.log(arguments)
-    app.config(["$routeProvider",function($routeProvider){
+    app.config(["$routeProvider", function ($routeProvider) {
 
     }])
 });
